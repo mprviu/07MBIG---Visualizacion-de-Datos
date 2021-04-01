aa44dbbd9d2d1f5b19d19294f2c8670a975eaf03
mprviu <mprviu@gmail.com>
2021-04-01 22:05:35 +0200

    2 files modified

--- ag03.js
+++ ag03.js
@@ -1,13 +1,18 @@
+//import "./styles.css";
 
-// Gráfico de columnas.
-var visualization = d3plus.viz()
+function load_graphic01() {
+  console.log("mec");
+  // Gráfico de columnas.
+  var visualization = d3plus
+    .viz()
     .container("#viz1")
     .data([
-        {"year": 1991, "name":"alpha", "value": 15},
-        {"year": 1992, "name":"alpha", "value": 20},
+      { year: 1991, name: "alpha", value: 15 },
+      { year: 1992, name: "alpha", value: 20 }
     ])
     .type("bar")
     .id("name")
     .x("year")
     .y("value")
-    .draw()
+    .draw();
+}
