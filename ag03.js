551d319ff29b5ca08b5d923fb9a13041961249b4
mprviu <mprviu@gmail.com>
2021-04-01 21:42:23 +0200

    1 file modified, 2 added

---
+++ ag03.js
@@ -0,0 +1,13 @@
+
+// Gráfico de columnas.
+var visualization = d3plus.viz()
+    .container("#viz1")
+    .data([
+        {"year": 1991, "name":"alpha", "value": 15},
+        {"year": 1992, "name":"alpha", "value": 20},
+    ])
+    .type("bar")
+    .id("name")
+    .x("year")
+    .y("value")
+    .draw()
